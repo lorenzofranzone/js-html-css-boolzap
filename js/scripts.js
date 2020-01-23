bb0ecc96c7fb0b2a4c78ae5e46eccccfefadc13c
Lorenzo Franzone <lorenzo.franzone@gmail.com>
2020-01-23 23:31:03 +0100

    Style Details
    Arrow search chat

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,26 @@ $(document).ready(function(){
 
   // Code
 
+  // Search Chat
+  var searchIf = true;
+  $(".all-chats-search input").on("click", function (event) {
+    $('.all-chats-search').addClass("bgw");
+    $('.all-chats-search i').attr("class","fas fa-arrow-left");
+    event.stopPropagation();
+    searchIf = false;
+  });
+  $("body").on("click", function () {
+    $('.all-chats-search').removeClass('bgw');
+    $('.all-chats-search i').attr("class","fas fa-search");
+  });
+
 
 
   //////////////////////////////////////////////////
   // FUNCTIONS
   //////////////////////////////////////////////////
 
-  
+
 
 /////
 }); // Document Ready
