b070205aa24c1aee83a2f3f6ba52fd6d3f4ff201
Lorenzo <lorenzo.franzone@gmail.com>
2020-01-24 19:09:40 +0100

    New Message in Chat

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   // Code
 
   // Search Chat
-
   $(".all-chats-search input").on("click", function (event) {
     $('.all-chats-search').addClass("bgw");
     $('.all-chats-search i').attr("class","fas fa-arrow-left");
@@ -23,14 +22,46 @@ $(document).ready(function(){
     $('ul.dropdown').removeClass('d-block');
   });
 
-
+  // Main Chat
+  $('.send-message').click(function(){
+    sendMessage();
+  });
 
 
   //////////////////////////////////////////////////
   // FUNCTIONS
   //////////////////////////////////////////////////
 
+  // FX Send New Message
+  function sendMessage() {
+    var textMessage = $('input.text-send-message').val();
 
+    if (textMessage.length != 0) {
+
+      var newMessage = $('.template .message').clone();
+      newMessage.children('p').text(textMessage);
+
+      var date = new Date();
+      var hours = date.getHours();
+      var minutes = addZero(date.getMinutes());
+      var time = hours + ':' + minutes;
+
+      newMessage.children('span').text(time);
+      newMessage.addClass('mine');
+
+      $('.block-single-chat').append(newMessage);
+
+      $('input.text-send-message').val('');
+    }
+  }
+
+  // FX Add Zero to Minutes < 10
+  function addZero(num) {
+    if (num < 10) {
+      num = '0' + num;
+    }
+    return num;
+  }
 
 /////
 }); // Document Ready
